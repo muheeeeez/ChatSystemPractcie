d9ceafc7cdb66ec9f3cac817b056ebeb63cbe867
Abdul-muiz Olaleye <abdulmuiz.olatunbosun@gmail.com>
2024-05-23 01:45:28 -0400

    first commit

---
+++ src/components/ChatView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Welcome, {{ username }}</div>
+</template>
+
+<script>
+
+import { ref } from "vue";
+export default {
+  props: ["username"],
+  setup(){
+    const inputMesssage = ref("");
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="view login">
+    <form class="login-form" @submit.prevent="registerUser">
+      <div class="form-inner">
+        <h1>Login to Smart Brains</h1>
+        <label for="username">Username</label>
+        <input
+          type="text"
+          v-model="state.username"
+          placeholder="Please enter your username"
+        />
+        <label for="email">Email</label>
+        <input
+          type="email"
+          v-model="state.email"
+          placeholder="Enter your email"
+        />
+        <label for="password">Password</label>
+        <input
+          type="password"
+          v-model="state.password"
+          placeholder="Enter your password"
+        />
+        <input type="submit" value="Login" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "vue-router";
+import ChatView from "./ChatView.vue";
+export default {
+  components: { ChatView },
+  setup() {
+    const state = reactive({
+      username: "",
+      email: "",
+      password: "",
+    });
+    const router = useRouter();
+    const registerUser = () => {
+      const username = state.username;
+      const email = state.email;
+      const password = state.password;
+      createUserWithEmailAndPassword(getAuth(), email, password)
+        .then((data) => {
+          console.log("Successfully Registered");
+          //   router.push("/chat");
+          router.push({ name: "chat", params: { username } });
+        })
+        .catch((error) => {
+          console.log(error.code);
+          alert(error.message);
+        });
+    };
+
+    return {
+      state,
+      registerUser,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.view {
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #ea526f;
+}
+
+.view.login {
+  align-items: center;
+}
+
+.view.login .login-form {
+  display: block;
+  width: 100%;
+  padding: 15px;
+}
+
+.view.login .login-form .form-inner {
+  display: block;
+  background-color: #fff;
+  padding: 50px 15px;
+  border-radius: 16px;
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
+}
+
+.view.login .login-form .form-inner h1 {
+  color: #aaa;
+  font-size: 28px;
+  margin-bottom: 30px;
+}
+
+.view.login .login-form .form-inner label {
+  display: block;
+  margin-bottom: 5px;
+  color: #aaa;
+  font-size: 16px;
+  transition: 0.4s;
+}
+
+.view.login .login-form .form-inner input[type="text"],
+input[type="email"],
+input[type="password"] {
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  display: block;
+  width: 100%;
+  padding: 10px 15px;
+  border-radius: 8px;
+  margin-bottom: 15px;
+  color: #333;
+  font-size: 18px;
+  box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
+  background-color: #f3f3f3;
+  transition: 0.4s;
+}
+
+.view.login .login-form .form-inner input[type="text"],
+input[type="email"],
+input[type="password"]::placeholder {
+  color: #888;
+  transition: 0.4s;
+}
+
+.view.login .login-form .form-inner input[type="submit"] {
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  display: block;
+  width: 100%;
+  padding: 10px 15px;
+  background-color: #ea526f;
+  border-radius: 8px;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+}
+
+.view.login .login-form .form-inner:focus-within label {
+  color: #ea526f;
+}
+
+.view.login .login-form .form-inner:focus-within input[type="text"],
+input[type="email"],
+input[type="password"] {
+  background-color: #fff;
+  box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
+}
+
+.view.login .login-form .form-inner:focus-within input[type="text"],
+input[type="email"],
+input[type="password"] ::placeholder {
+  color: #666;
+}
+</style>
